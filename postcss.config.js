c8c5757b3e7272a6af24374d02451e0e40d7ecc7
ssuhacho <ssuhacho@gmail.com>
2022-06-22 23:56:58 -0500

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+// const tailwindcss = require('tailwindcss');
+
+// module.exports = {
+//     plugins: [
+//         tailwindcss('./tailwind.js'),
+//         require('autoprefixer')
+//     ]
+// }
+
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
